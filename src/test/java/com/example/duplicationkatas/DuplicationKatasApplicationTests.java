8421ee10034e5dde76f078e8f362b167d25d8c0a
dtatiana <dtatiana@vmware.com>
2023-03-24 10:55:27 -0400

    initial commit

---
+++ src/test/java/com/example/duplicationkatas/DuplicationKatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duplicationkatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicationKatasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
